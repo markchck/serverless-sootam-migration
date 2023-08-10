cdea5455304150960a8d17142f93676a0eb0c783
markchck <markchck@gmail.com>
2023-08-10 18:12:41 +0900

    feat - mysql -> dynamo로 마이그레이션 로직 완성

--- migration.js
+++ migration.js
@@ -4,44 +4,66 @@ const AWS = require("aws-sdk")
 const AWS_region = "ap-northeast-2"
 AWS.config.update({ region: AWS_region })
 
-async function putDB() {
+// async function putDB() {}
+
+async function extract() {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, "utf-8", (error, data) => {
+      if (error) {
+        console.error("파일 읽기 오류:", error)
+        reject(error)
+        return
+      }
+      resolve(data)
+    })
+  })
+}
+
+async function main() {
   try {
-    const dynamoDb = new AWS.DynamoDB.DocumentClient()
-    const putParams = {
-      TableName: "math_platform_development",
-      Item: {
-        pk: "hi",
-        sk: "hello",
-      },
-    }
-    const response = await dynamoDb.put(putParams).promise()
-    return response
+    const data = await extract()
+    JSON.parse(data).forEach(async (element) => {
+      try {
+        const questionId = element.id || ""
+        const year = element.date.slice(0, 4) || ""
+        const month = element.date.slice(4, 6) || ""
+        const copyright = element.copyright.split(" ")[0] || ""
+        const testType = element.copyright.split(" ")[1] || ""
+        const number = element.number.toString() || ""
+        const successRate = element.corper.toString() || ""
+        const imageData = JSON.parse(element.image_data).id || ""
+        const solutionData = JSON.parse(element.solution_data).id || ""
+        const answer = element.answer.toString() || ""
+
+        const dynamoDb = new AWS.DynamoDB.DocumentClient()
+        const putParams = {
+          TableName: "sootam-dev",
+          Item: {
+            pk: "question",
+            sk: `questionId#${questionId}`,
+            year: year,
+            month: month,
+            number: number,
+            successRate: successRate,
+            imageData: imageData,
+            solutionData: solutionData,
+            answer: answer,
+            testType: testType,
+            copyright: copyright,
+            createdAt: new Date().toISOString(),
+            updatedAt: new Date().toISOString(),
+          },
+        }
+        const response = await dynamoDb.put(putParams).promise()
+        return response
+      } catch (error) {
+        console.error("에러 발생:", error)
+      }
+    })
   } catch (error) {
     console.error("에러 발생:", error)
   }
 }
 
-// async function extract() {
-//   return new Promise((resolve, reject) => {
-//     fs.readFile(filePath, "utf-8", (error, data) => {
-//       if (error) {
-//         console.error("파일 읽기 오류:", error)
-//         reject(error)
-//         return
-//       }
-//       resolve(data)
-//     })
-//   })
-// }
-
-// async function main() {
-//   try {
-//     const data = await extract()
-//     console.log(data)
-//   } catch (error) {
-//     console.error("에러 발생:", error)
-//   }
-// }
-
-// main()
-putDB()
+main()
+// putDB()
